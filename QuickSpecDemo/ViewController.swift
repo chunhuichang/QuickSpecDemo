59f3dbb31927ffef77b43783587061d5d02812d5
張淳惠 <jillchang@cathaylife.com.tw>
2020-06-11 13:46:17 +0800

    Initial Commit

---
+++ QuickSpecDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuickSpecDemo
+//
+//  Created by i9400519 on 2020/6/11.
+//  Copyright © 2020 i9400519. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
